032f6995cea3252e15082564f063584b92cb1a07
jzeff <jzeffcode@gmail.com>
2023-09-29 21:39:04 +0800

    put into utility files for getting permission and for setting up notification channel, and fix minor codes.

---
+++ src/utils/setupNotifChannel.js
@@ -0,0 +1,12 @@
+import { Platform } from "react-native";
+
+export const setupNotifChannel = async () => {
+  if (Platform.OS === "android") {
+    await Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+};
